eb83fa27883560017385447bbb1641234eee0166
Ahmmed Hashim <ahmedhasim209>
2023-08-03 01:37:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let icon = document.getElementById("burger");
+let ul = document.getElementById("ul");
+let ex = document.getElementById("ex");
+
+
+icon.onclick = function () {
+    ul.style.display = "block"
+    icon.style.display = "none"
+    ex.style.display = "block"
+}
+ex.onclick = function () {
+    ul.style.display = "none"
+    icon.style.display = "block"
+}
